215aaa48aaa379098b711b01af0c96ec9e572923
Greetings-Hello-Hi <181512451+Greetings-Hello-Hi@users.noreply.github.com>
2025-05-05 18:27:09 -0500

    Write function for repeating strings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    } else if (num == 0) {
+        return '';
+    } else {
+        let newString = '';
+        for (repeats = 1; repeats <= num; repeats++) {
+            newString += string;
+        }
+        return newString;
+    }
 };
 
 // Do not edit below this line
