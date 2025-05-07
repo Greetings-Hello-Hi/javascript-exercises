da16e58360e001c7bbdd538fa9746726a973b717
Greetings-Hello-Hi <181512451+Greetings-Hello-Hi@users.noreply.github.com>
2025-05-06 20:57:01 -0500

    Create reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(origString) {
+    let stringLength = origString.length;
+    let newString = '';
+    for (let i = stringLength - 1; i >= 0; i--) {
+        let character = origString.charAt(i);
+        newString += character;
+    }
+    return newString;
 };
 
 // Do not edit below this line
